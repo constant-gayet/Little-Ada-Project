9d1dde4add3bfcf64c5cdb8f9d334000610f6616
constant-gayet <76485344+constant-gayet@users.noreply.github.com>
2021-12-22 19:37:53 +0100

    type definitions

---
+++ little_ada.c
@@ -0,0 +1,37 @@
+typedef struct commentaire
+{
+
+} commentaire;
+typedef struct identifiant
+{
+
+} identifiant;
+
+typedef struct constante
+{
+    //decimale, avec base ou de chaines de caracteres
+} constante;
+
+typedef struct expression
+{
+    /* identifiant (qualifié ou non)
+        constante 
+        expression précédée de - ou abs ou not
+        ...
+        voir sujet 
+         */
+} expression;
+
+typedef struct file
+{
+    // pas trouvé dans le sujet ???
+} file;
+
+typedef struct declaration
+{
+} declaration;
+
+typedef struct instruction
+{
+
+} instruction;
